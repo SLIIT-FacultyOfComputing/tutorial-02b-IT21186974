77cde334854d03d17eaa66a67f491a2b56bcdb75
IT21186974 <99704368+IT21186974@users.noreply.github.com>
2022-02-22 08:13:16 +0000

    Done all the 4 Exercises in tutorial 2. (Completed coding)

--- tute02.cpp
+++ tute02.cpp
@@ -12,33 +12,50 @@ Type = 3
 OtRate = 1700
 Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
 
-#include <stdio.h>
+
+#include <iostream>
+using namespace std;
+ 
 int main()
 {
-   double salary, netSalary;
-   int etype, otHrs, otRate;
-   printf("Enter Employee Type : ");
-   scanf("%d", &etype);
-   printf("Enter Salary  : ");
-   scanf("%f", &salary);
-   printf("Enter OtHrs : ");
-   scanf("%d", &otHrs); 
-   
-   switch (etype) {
-      case 1 :
-          otRate = 1000;
-          break;
-      case 2 :
-          otRate = 1500;
-          break;
-      default :
-          otRate = 1700;
-          break;
-   }
-
-
-   netSalary = salary + otHrs* otRate;
-   printf("Net Salary is %f ", netSalary);
-  
-   return 0;
+ double salary, netSalary;
+ int etype, otHrs, otRate;
+ 
+ //printf("Enter Employee Type : ");
+ cout << "Enter Employee Type : ";
+ //scanf("%d", &etype);
+ cin >> etype ;
+ cout << endl;
+ 
+ //printf("Enter Salary : ");
+ cout << "Enter Salary : " ;
+ //scanf("%f", &salary);
+ cin >> salary ;
+ cout << endl;
+ 
+ //printf("Enter OtHrs : ");
+ cout << "Enter OT Hours : " ;
+ //scanf("%d", &otHrs); 
+ cin >> otHrs ;
+ cout << endl;
+ 
+ switch (etype) {
+ 					case 1 :
+						otRate = 1000;
+ 						break;
+					case 2 :
+ 						otRate = 1500;
+ 						break;
+ 					default :
+ 						otRate = 1700;
+ 						break;
+ 				}
+ 				
+ netSalary = salary + otHrs* otRate;
+ 
+ cout << endl;
+ //printf("Net Salary is %f ", netSalary);
+ cout << "Net Salary is : " << netSalary << endl ;
+ 
+ return 0;
 }
--- tute04.cpp
+++ tute04.cpp
@@ -13,17 +13,50 @@ Do not modify the main function.*/
 
 #include <iostream>
 
+//function prototyping
 long Factorial(int no);
 long nCr(int n, int r);
 
 int main() {
-  int n, r;
-  std::cout << "Enter a value for n ";
-  std::cin >> n;
-  std::cout << "Enter a value for r ";
-  std::cin >> r;
-  std::cout << "nCr = ";
-  std::cout << nCr(n,r);
-  std::cout << std::endl;
-  return 0;
+ int n, r;
+
+ std::cout << "Enter a value for n ";
+ std::cin >> n;
+ std::cout << "Enter a value for r ";
+ std::cin >> r;
+
+ std::cout << "nCr = ";
+
+ std::cout << nCr(n,r);
+ std::cout << std::endl;
+
+return 0;
+}
+
+//implimentation of nCr function
+long nCr (int n, int r)
+{
+	int x = (n-r);
+	long nCr;
+	
+	Factorial(n);
+	Factorial(r);
+	Factorial(x);
+	
+	nCr = Factorial(n) / (Factorial(r)*Factorial(x));
+	
+	return nCr;
+}
+
+//implimentation of Factorial function
+long Factorial(int no)
+{
+	int y=1;
+	
+ 	for (no; no >= 1; no--) 
+ 		{
+ 			y = y * no; //factorial calculation
+ 		}
+ 		
+ 	return y;	
 }
